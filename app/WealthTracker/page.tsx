796cec615b6cf05ed2b140d16064074711cde3c8
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-10-15 19:08:10 +0300

    Updating Routing

---
+++ app/WealthTracker/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WealthTracker = () => {
+  return <div>Wealth Tracker</div>;
+};
+
+export default WealthTracker;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-import Healthtracker from "./components/Healthtracker";
-import Gallery from "./components/Gallery";
+import Link from "next/link";
+import Healthtracker from "./Healthtracker/page";
+import Gallery from "./Gallery/page";
 
 const page = () => {
   return (
     <div>
-      <Gallery />
-      <Healthtracker />
+      <Link href="/" className="text-3xl font-bold text-center block my-6">
+        GAIA Genesis
+      </Link>
+
+      <Link
+        href="/Gallery"
+        className="text-blue-500 underline block text-center mb-6"
+      >
+        View Gallery
+      </Link>
+      <Link
+        href="/Healthtracker"
+        className="text-blue-500 underline block text-center mb-6"
+      >
+        View Health Tracker
+      </Link>
 
       <footer className="text-center text-gray-500 py-6">
         GAIA Genesis © 2025 — Built with Next.js & Tailwind
